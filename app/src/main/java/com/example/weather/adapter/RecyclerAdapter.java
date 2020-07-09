1e7a0dc45ea2605031657b6425137da7687653e3
gakha <gakhartanvi@gmail.com>
2020-07-08 20:32:04 -0400

    In recycleAdapter layout inflate done and methods implementation done.

--- app/src/main/java/com/example/weather/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/example/weather/adapter/RecyclerAdapter.java
@@ -1,4 +1,38 @@
 package com.example.weather.adapter;
 
-public class RecyclerAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.weather.R;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
+
+    @NonNull
+    @Override
+    public RecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleitem,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        public ViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+        }
+    }
 }
