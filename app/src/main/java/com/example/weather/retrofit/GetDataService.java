d60080a0f6f657df0be62ccad53b22e9c75b0f67
gakha <gakhartanvi@gmail.com>
2020-07-08 20:40:10 -0400

    In retrofit package code for GetDataService and RetrofitClientInstance.java updated with base url and end point of montreal for getting data from API.

--- app/src/main/java/com/example/weather/retrofit/GetDataService.java
+++ app/src/main/java/com/example/weather/retrofit/GetDataService.java
@@ -1,4 +1,12 @@
 package com.example.weather.retrofit;
 
+import com.example.weather.POJO.ConsolidatedWeather;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface GetDataService {
+
+    @GET("3534")
+    Call<ConsolidatedWeather> getMontrealWeatherData();
 }
