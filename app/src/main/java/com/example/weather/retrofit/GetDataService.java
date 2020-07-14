9f1a3f399f62c0296efc85e19020f3eac2f8b3c1
Kamal <saikamaljit181294@gmail.com>
2020-07-14 12:12:55 -0400

    More cities are added in menu and endpoint with woeid of these cities are added in GetDataService class.

--- app/src/main/java/com/example/weather/retrofit/GetDataService.java
+++ app/src/main/java/com/example/weather/retrofit/GetDataService.java
@@ -9,4 +9,28 @@ public interface GetDataService {
 
     @GET("3534")
     Call<ConsolidatedWeather> getMontrealWeatherData();
+
+    @GET("44418")
+    Call<ConsolidatedWeather> getLondonWeatherData();
+
+    @GET("2379574")
+    Call<ConsolidatedWeather> getChicagoWeatherData();
+
+    @GET("2295402")
+    Call<ConsolidatedWeather> getAhmedabadWeatherData();
+
+    @GET("2459115")
+    Call<ConsolidatedWeather> getNewyorkWeatherData();
+
+    @GET("4118")
+    Call<ConsolidatedWeather> getTorontoWeatherData();
+
+    @GET("9807")
+    Call<ConsolidatedWeather> getVancouverWeatherData();
+
+    @GET("12586539")
+    Call<ConsolidatedWeather> getMumbaiWeatherData();
+
+    @GET("2295405")
+    Call<ConsolidatedWeather> getSuratWeatherData();
 }
