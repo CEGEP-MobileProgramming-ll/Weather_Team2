91770e90998b6548dcc4ff7765397d4ee0b6fc62
gakha <gakhartanvi@gmail.com>
2020-07-13 22:44:50 -0400

    loadJson method for loading data from api and generateview method for generating view using adapter added.

--- app/src/main/java/com/example/weather/MainFragment.java
+++ app/src/main/java/com/example/weather/MainFragment.java
@@ -5,18 +5,25 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.weather.POJO.ConsolidatedWeather;
 import com.example.weather.POJO.ConsolidatedWeather_;
 import com.example.weather.adapter.RecyclerAdapter;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainFragment extends Fragment {
 
@@ -29,12 +36,9 @@ public class MainFragment extends Fragment {
         // Required empty public constructor
     }
 
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -48,6 +52,8 @@ public class MainFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
+
+
     public boolean checkInternetConnection() {
 
         //Check internet connection:
@@ -59,4 +65,41 @@ public class MainFragment extends Fragment {
 
         return connected;
     }
+
+    public void loadJson(Call<ConsolidatedWeather> call){
+        call.enqueue(new Callback<ConsolidatedWeather>() {
+            @Override
+            public void onResponse(Call<ConsolidatedWeather> call, Response<ConsolidatedWeather> response) {
+                System.out.println("Response From URL :" + response.body());
+                try {
+                    ConsolidatedWeather weather = response.body();
+                    wArrayList = new ArrayList<>(weather.getConsolidatedWeather());
+                    generateView(wArrayList);
+
+                }catch (NullPointerException e)
+                {
+                    System.out.println("Nullpointer Exception :"+e.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ConsolidatedWeather> call, Throwable t) {
+
+                System.out.println("In Failure :" + t.getMessage());
+
+            }
+        });
+    }
+
+    public void generateView(ArrayList<ConsolidatedWeather_> weatherS)
+    {
+
+        recyclerAdapter = new RecyclerAdapter(weatherS, getActivity().getApplicationContext());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.VERTICAL,false);
+        recyclerView = getView().findViewById(R.id.main_recyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(recyclerAdapter);
+    }
+
+
 }
